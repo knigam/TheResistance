01cf7a611e5233376c0eb75c0b6199ad79d41c28
knigam <kushal.nigam1@gmail.com>
2014-01-12 02:19:01 -0500

    added main screen with new game/load game options

--- The Resistance/src/main/java/com/keonasoft/resistance/MainScreen.java
+++ The Resistance/src/main/java/com/keonasoft/resistance/MainScreen.java
@@ -18,11 +18,11 @@ public class MainScreen extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        if (savedInstanceState == null) {
+        /*if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-        }
+        }*/
     }
 
 
@@ -46,9 +46,9 @@ public class MainScreen extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
+ /*   *//**
      * A placeholder fragment containing a simple view.
-     */
+     *//*
     public static class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
@@ -60,6 +60,6 @@ public class MainScreen extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_main_screen, container, false);
             return rootView;
         }
-    }
+    }*/
 
 }
